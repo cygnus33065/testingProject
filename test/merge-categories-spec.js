a1751c3bead00919870c51973012a61e3dde367b
John Beck <cygnus_33065@yahoo.com>
2020-12-11 16:07:21 -0500

    merge tests

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -12,14 +12,36 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <li>s for no categories", () => {
+      const catagories = []
+      const result = mergeCategories(template, catagories, 'li')
+      expect(result).to.contain('<div>')
+      expect(result).to.contain('</div>')
+      expect(result).to.contain('<ul>')
+      expect(result).to.contain('</ul>')
+      expect(result).to.not.contain('<li>')
+      expect(result).to.not.contain('</li>')
     });
 
     it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
+      const catagories = ['1']
+      const result = mergeCategories(template, catagories, 'li')
+      expect(result).to.contain('<div>')
+      expect(result).to.contain('</div>')
+      expect(result).to.contain('<ul>')
+      expect(result).to.contain('</ul>')
+      expect(result).to.contain('<li>1</li>')
+
     });
 
     it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
+      const catagories = ['1', '2']
+      const result = mergeCategories(template, catagories, 'li')
+      expect(result).to.contain('<div>')
+      expect(result).to.contain('</div>')
+      expect(result).to.contain('<ul>')
+      expect(result).to.contain('</ul>')
+      expect(result).to.contain('<li>1</li>')
+      expect(result).to.contain('<li>2</li>')
     });
   });
 
